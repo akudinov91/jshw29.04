40cb7459e2f55878653ddee4404e29c16268c35a
akudinov91 <andrewkudinov13@gmail.com>
2024-04-29 19:48:46 +0400

    finished

---
+++ 2/index.js
@@ -0,0 +1,19 @@
+// function changeBackgroundColor() {
+//   const scrollPosition = window.scrollY;
+
+//   const threshold1 = 500;
+//   const threshold2 = 1000;
+//   const color1 = "#f0f0f0";
+//   const color2 = "#b0e0e6";
+//   const color3 = "#ffd700";
+
+//   if (scrollPosition < threshold1) {
+//     document.body.style.backgroundColor = color1;
+//   } else if (scrollPosition >= threshold1 && scrollPosition < threshold2) {
+//     document.body.style.backgroundColor = color2;
+//   } else {
+//     document.body.style.backgroundColor = color3;
+//   }
+// }
+
+// window.addEventListener("scroll", changeBackgroundColor);
